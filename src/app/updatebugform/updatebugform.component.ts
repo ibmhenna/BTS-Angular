494f7b6919a681ef3f39a86a7dc184594f999a32
unknown <ibmjavanh11@iiht.tech>
2021-04-22 18:21:26 +0530

    changes in update bug

--- src/app/updatebugform/updatebugform.component.ts
+++ src/app/updatebugform/updatebugform.component.ts
@@ -21,6 +21,7 @@ export class UpdatebugformComponent implements OnInit {
       console.log(response);
         this.bugArray=[response];
         console.log("success");
+        console.log(this.bug.id);
       },
       error=>{
         console.log(error);
@@ -28,9 +29,20 @@ export class UpdatebugformComponent implements OnInit {
       })
   }
 
-  update(){
+  update() {
+    const promise = this.bugService.updateBug(this.bug, this.bug.id);
+    promise.subscribe((response: any) => {
+      console.log(response);
+      alert('Bug is Updated')
 
+    },
+      error => {
+        console.log(error);
+        alert('Error Occured')
+
+      })
   }
+
 
   ngOnInit(): void {
   }
